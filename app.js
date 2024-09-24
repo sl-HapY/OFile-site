ef647b99dd155cd50680d3314f2fde2b1c927cec
HapY <152333804+sl-HapY@users.noreply.github.com>
2024-09-25 00:54:55 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function copyText() {
     const textToCopy = "0x865589ECafC23fa9E3B872e95Dd08e27188d6a7A";
     const textarea = document.createElement("textarea");
@@ -7,8 +6,5 @@ function copyText() {
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
-    document.getElementById("message").innerText = "Tether address copied";
-    document.getElementById("message").style.display = "block";
-    setTimeout(() => {document.getElementById("message").style.display = "none";}, 2000);
 }
 document.getElementById("qrCodeImage").addEventListener("mousedown", copyText);
